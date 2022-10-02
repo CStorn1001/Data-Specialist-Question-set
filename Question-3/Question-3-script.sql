2de907259ef4e462c3a1ae2391665c4e2151bc92
CStorn1001 <sc1001@tutanota.com>
2022-10-03 09:50:08 +1100

    final solution

--- Question-3/Question-3-script.sql
+++ Question-3/Question-3-script.sql
@@ -4,30 +4,22 @@ use q3;
 
 -- had to import the csv file via 'import wizard' on mysql  
 
--- updating the CostDate field from text to date
+-- updating the CostDate field from text to date as import wizard only gave a datetime datatype
 UPDATE `question 3 data`
 SET CostDate = DATE_FORMAT( STR_TO_DATE(CostDate,"%d/%m/%Y" ) ,"%Y/%m/%d" );
 
 -- testing to see if data view executes correctly
-select * from `question 3 data` order by Item asc, CostDate desc; 
+select * from `question 3 data`; 
 
--- selecting statements for PreviousCost and Cost range are completed through a nested query within the select statement. This is done to determine the previous date after the current date.
--- I use a limit of 1 as it will select the 1st record which is the one of interest
--- I use a nested query inside the from clause so I can filter the data by items ascending and by most current dates
--- finally I group by the item to grab the most current item.
-select c.Item, c.Cost as 'CurrentCost' , c.CostDate as 'CurrentCostDate' , 
-(select c2.Cost from `question 3 data` c2 where c2.Item = c.Item and c.CostDate > c2.CostDate order by c2.CostDate desc limit 1) as PreviousCost, 
-(c.Cost - (select c2.Cost from `question 3 data` c2 where c2.Item = c.Item and c.CostDate > c2.CostDate order by c2.CostDate desc limit 1)) as 'CostChange'
-from (select * from `question 3 data` order by Item asc, CostDate desc)  as c;
--- group by c.Item;
+-- Selecting the previouscost and change through a select query in which I determine the 2nd earliest date for each item. LIMIT is used to grab the first record of the query
+-- The Cost change uses the same query from previous cost and the earlier cost to get the difference rounded by 2 decmial places
+-- Within the WHERE Clause I try to get the max record for CostDate per item
+-- Finally I order the query by Items ascending and costdate descending
+SELECT c.Item, c.Cost AS 'CurrentCost' , c.CostDate AS 'CurrentCostDate' , 
+(SELECT c2.Cost FROM `question 3 data` c2 WHERE c2.Item = c.Item AND c.CostDate > c2.CostDate ORDER BY c2.CostDate DESC LIMIT 1) AS 'PreviousCost', 
+ROUND(c.Cost - (SELECT c2.Cost FROM `question 3 data` c2 WHERE c2.Item = c.Item and c.CostDate > c2.CostDate ORDER BY c2.CostDate DESC LIMIT 1),2) AS 'CostChange'
+FROM `question 3 data` c
+WHERE c.CostDate=  (SELECT MAX(c3.CostDate) FROM `question 3 data` c3 WHERE c.Item = c3.Item)
+ORDER BY c.Item ASC, c.CostDate DESC;
 
---
-select c.Item, c.Cost as 'CurrentCost' , c.CostDate as 'CurrentCostDate' , 
-(select c2.Cost from `question 3 data` c2 where c2.Item = c.Item and c.CostDate > c2.CostDate order by c2.CostDate desc limit 1) as PreviousCost, 
-(c.Cost - (select c2.Cost from `question 3 data` c2 where c2.Item = c.Item and c.CostDate > c2.CostDate order by c2.CostDate desc limit 1)) as 'CostChange'
-from `question 3 data` c
-group by c.Item
-order by Item asc, CostDate asc;
-
-
-select * from `question 3 data`;
+-- Solution output can be found under csv file 'Q3-output.csv'
---
+++ Question-3/README.md
@@ -0,0 +1,5 @@
+# Data-Specialist-Question-set
+Data Addiction- Data Specialist Interview Questions
+* Q1: Completed via py file and solution within text file
+* Q2: Completed via jupiter notebook file and solution within json file
+* Q3: Completed via MYSQL (as already on my device and is my most confident SQL type) and solution is within a csv file
